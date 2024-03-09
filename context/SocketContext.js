6f7ff1a2472d1b0e779077c664f14ffdb98eeb98
ridoy <ridoy51306@gmail.com>
2024-03-09 02:30:30 -0500

    changes

--- context/SocketContext.js
+++ context/SocketContext.js
@@ -67,7 +67,7 @@ const SocketContext = ({ children }) => {
           setSocket(null);
         }
       };
-    }, setState);
+    }, setState); 
     return () => {};
   };
   useEffect(socketInitializer, [socket, state.user?.id]);
